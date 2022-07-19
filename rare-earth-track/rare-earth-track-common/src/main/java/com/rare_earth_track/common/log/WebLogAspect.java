82c8532052071e67e066053377bc07944dac9ce8
hhoao <haunghao@foxmail.com>
2022-07-19 18:55:32 +0800

    feat(log): 添加简单日志处理功能

---
+++ rare-earth-track/rare-earth-track-common/src/main/java/com/rare_earth_track/common/log/WebLogAspect.java
@@ -0,0 +1,109 @@
+package com.rare_earth_track.common.log;
+
+import cn.hutool.json.JSONUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 简单统一日志处理切面
+ * Created by macro on 2018/4/26.
+ */
+@Aspect
+@Component
+@Order(1)
+public class WebLogAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebLogAspect.class);
+
+    @Pointcut("execution(public * com.rare_earth_track.*.controller.*.*(..))||execution(public * com.rare_earth_track.*.controller.*.*(..))")
+    public void webLog() {
+    }
+
+    @Before("webLog()")
+    public void doBefore() {
+    }
+
+    @Around("webLog()")
+    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        //获取当前请求对象
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        assert attributes != null;
+        HttpServletRequest request = attributes.getRequest();
+        Object result = joinPoint.proceed();
+        Signature signature = joinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature) signature;
+        Method method = methodSignature.getMethod();
+        Map<String, Object> logMap = new HashMap<>();
+        if (method.isAnnotationPresent(Operation.class)) {
+            Operation log = method.getAnnotation(Operation.class);
+            logMap.put("description", log.summary());
+        }
+        long endTime = System.currentTimeMillis();
+        logMap.put("username", request.getRemoteUser());
+        logMap.put("ip", request.getRemoteAddr());
+        logMap.put("method", request.getMethod());
+        logMap.put("parameter", getParameter(method, joinPoint.getArgs()));
+        logMap.put("url", request.getRequestURL().toString());
+        logMap.put("spendTime", (int) (endTime - startTime));
+        logMap.put("result", result);
+        logMap.put("startTime", startTime);
+        LOGGER.info(JSONUtil.parse(logMap).toString());
+        return result;
+    }
+
+    /**
+     * 根据方法和传入的参数获取请求参数
+     */
+    private Object getParameter(Method method, Object[] args) {
+        List<Object> argList = new ArrayList<>();
+        Parameter[] parameters = method.getParameters();
+        for (int i = 0; i < parameters.length; i++) {
+            //将RequestBody注解修饰的参数作为请求参数
+            RequestBody requestBody = parameters[i].getAnnotation(RequestBody.class);
+            if (requestBody != null) {
+                argList.add(args[i]);
+            }
+            //将RequestParam注解修饰的参数作为请求参数
+            RequestParam requestParam = parameters[i].getAnnotation(RequestParam.class);
+            if (requestParam != null) {
+                Map<String, Object> map = new HashMap<>();
+                String key = parameters[i].getName();
+                if (!StringUtils.hasLength(requestParam.value())) {
+                    key = requestParam.value();
+                }
+                map.put(key, args[i]);
+                argList.add(map);
+            }
+        }
+        if (argList.size() == 0) {
+            return null;
+        } else if (argList.size() == 1) {
+            return argList.get(0);
+        } else {
+            return argList;
+        }
+    }
+}
